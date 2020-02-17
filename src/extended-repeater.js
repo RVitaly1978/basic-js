79c660540342c9917d561b1b88b0fe4bc92e3796
Vitaly Raicheu <RVitaly1978@gmail.com>
2020-02-17 15:05:53 +0300

    feat: solve extended repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,25 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let baseStr = String(str);
+    let repeatTimes = options.repeatTimes || 1;
+    let separator = options.separator || '+';
+    let addition = String(options.addition);
+    let additionRepeatTimes = options.additionRepeatTimes || 0;
+    let additionSeparator = options.additionSeparator || '|';
+
+    function makeRepeatingStr(repStr, repTimes, repSeparator) {
+        if (repStr === 'undefined') {
+            return '';
+        }
+
+        if ((!Number.isInteger(repTimes))
+            || repTimes <= 0) {
+            return repStr;
+        }
+
+        return Array(repTimes).fill(repStr).join(repSeparator);
+    }
+
+    const fullStr = baseStr + makeRepeatingStr(addition, additionRepeatTimes, additionSeparator);
+    return makeRepeatingStr(fullStr, repeatTimes, separator);
 };
   
