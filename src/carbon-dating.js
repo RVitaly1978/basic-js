4aa08b472484d8c98a9bfe2ca4e98ee97693e733
Vitaly Raicheu <RVitaly1978@gmail.com>
2020-02-18 13:43:34 +0300

    feat: add carbon dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,19 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15; 
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (arguments.length === 0)  return false;
+
+  if (typeof sampleActivity !== 'string') return false;
+
+  const validateSampleActivity = Number.parseFloat(sampleActivity);
+
+  if (Number.isNaN(validateSampleActivity)) return false;
+
+  if ((validateSampleActivity <= 0) || (validateSampleActivity > MODERN_ACTIVITY)) return false;
+
+  const passedTime = (Math.log(MODERN_ACTIVITY / validateSampleActivity) * HALF_LIFE_PERIOD) / 0.693;
+  const roundedPassedTime = Math.ceil(passedTime);
+
+  return roundedPassedTime;
 };
