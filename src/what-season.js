cc8407a990e97f70437f741ecd85ca8ac14d4c30
Vitaly Raicheu <RVitaly1978@gmail.com>
2020-02-18 15:30:22 +0300

    feat: add what season task solution

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,27 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  const WINTER = 'winter';
+  const SPRING = 'spring';
+  const SUMMER = 'summer';
+  const AUTUMN = 'autumn';
+
+  if (arguments.length === 0) {
+    return 'Unable to determine the time of year!';
+  }
+
+  if (!(date instanceof Date)) throw new Error();
+  
+  if (!date.getTime()) throw new Error();
+
+  const month = date.getMonth();
+  let season = WINTER;
+
+  if ((month > 1) && (month < 5)) {
+    season = SPRING;
+  } else if ((month > 4) && (month < 8)) {
+    season = SUMMER;
+  } else if ((month > 7) && (month < 11)) {
+    season = AUTUMN;
+  }
+
+  return season;
 };
